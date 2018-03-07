44ce746af7cc08f76c3112596db3903ca5e97a5c
Matt McElwee <mmcelwee@dmsi.com>
2018-03-07 12:19:30 -0600

    Added boilerplate for form handling

---
+++ public/js/form.js
@@ -0,0 +1,5 @@
+document.getElementById('save').addEventListener('submit', e => {
+	e.preventDefault();
+	gtag('event', 'conversion', {'send_to': 'AW-826716326/kq3ICOr5y34QpuGaigM'});
+	return false;
+});
