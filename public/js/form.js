72d66cf758948404f770758d6034ddd9b79e04cc
Matt McElwee <mubatt@wyopub.com>
2018-03-07 14:56:08 -0500

    Made minor tracking and useability changes - Added the muted attribute to the video element to force autoplay on Webkit browsers - Added google analytics tracking

--- public/js/form.js
+++ public/js/form.js
@@ -1,5 +1,15 @@
-document.getElementById('save').addEventListener('submit', e => {
-	e.preventDefault();
-	gtag('event', 'conversion', {'send_to': 'AW-826716326/kq3ICOr5y34QpuGaigM'});
-	return false;
-});
+window.onload = () => {
+	document.getElementById('save').addEventListener('submit', e => {
+		e.preventDefault();
+		gtag('event', 'conversion', {'send_to': 'AW-826716326/kq3ICOr5y34QpuGaigM'});
+
+		const data = {
+			firstName: document.getElementById('first-name').value,
+			lastName: document.getElementById('last-name').value,
+			email: document.getElementById('email').value,
+			number: parseInt(document.getElementById('number').value, 10)
+		};
+
+		return false;
+	});
+}
